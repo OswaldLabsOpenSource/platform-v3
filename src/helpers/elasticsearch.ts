c32695972bdbbca78b89bf111867abacadbc78b0
Anand Chowdhary <mail@anandchowdhary.com>
2020-02-18 17:49:03 +0100

    :wrench: Add .env DEV for Redis

--- src/helpers/elasticsearch.ts
+++ src/helpers/elasticsearch.ts
@@ -64,6 +64,7 @@ export const cleanElasticSearchQueryResponse = (
   response: any,
   size: number
 ) => {
+  console.log("Response", response);
   if (response.hits && response.hits.hits) {
     const count = response.hits.total;
     const data = response.hits.hits;
--- src/helpers/redis.ts
+++ src/helpers/redis.ts
@@ -1,27 +1,37 @@
 import { createHandyClient } from "handy-redis";
 import { logError } from "./errors";
 
-const redis = createHandyClient({
-  host: "srv-captain--redis",
-  port: 6379,
-  password: process.env.REDIS_PASSWORD,
-  retry_strategy: options => {
-    if (options.error && options.error.code === "ECONNREFUSED") {
-      logError("Redis connection failed", "Server refused the connection");
-    }
+const redis = createHandyClient(
+  process.env.DEV
+    ? undefined
+    : {
+        host: "srv-captain--redis",
+        port: 6379,
+        password: process.env.REDIS_PASSWORD,
+        retry_strategy: options => {
+          if (options.error && options.error.code === "ECONNREFUSED") {
+            logError(
+              "Redis connection failed",
+              "Server refused the connection"
+            );
+          }
 
-    if (options.total_retry_time > 1000 * 60 * 60) {
-      logError("Redis connection failed", "Total retry time exhausted");
-    }
+          if (options.total_retry_time > 1000 * 60 * 60) {
+            logError("Redis connection failed", "Total retry time exhausted");
+          }
 
-    if (options.attempt > 10) {
-      logError("Redis connection failed", "Max number of attempts exceeded");
-      return 43200;
-    }
+          if (options.attempt > 10) {
+            logError(
+              "Redis connection failed",
+              "Max number of attempts exceeded"
+            );
+            return 43200;
+          }
 
-    // Reconnect after this time
-    return Math.min(options.attempt * 100, 3000);
-  }
-});
+          // Reconnect after this time
+          return Math.min(options.attempt * 100, 3000);
+        }
+      }
+);
 
 export default redis;
