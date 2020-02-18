bcd96de121407403ffb631cccea6c33b9794562c
Anand Chowdhary <mail@anandchowdhary.com>
2020-02-18 12:24:27 +0100

    :recycle: Use SMTP to send emails

--- src/helpers/mail.ts
+++ src/helpers/mail.ts
@@ -1,10 +1,11 @@
-import { Response } from "request";
+import { createTransport } from "nodemailer";
 import { Mail } from "../interfaces/mail";
 import { FRONTEND_URL, TEST_EMAIL } from "../config";
 import { readFile } from "fs-extra";
 import { join } from "path";
 import { render } from "mustache";
 import marked from "marked";
+import { success, error } from "signale";
 import { isMatch } from "matcher";
 import disposableDomains from "disposable-email-domains/index.json";
 import wildcardDomains from "disposable-email-domains/wildcard.json";
@@ -17,22 +18,48 @@ import systemInfo from "systeminformation";
 import pkg from "../../package.json";
 
 const EMAIL_FROM = process.env.EMAIL_FROM || "";
+const EMAIL_HOST = process.env.EMAIL_HOST || "";
+const EMAIL_PASSWORD = process.env.EMAIL_PASSWORD || "";
+const transporter = createTransport(
+  {
+    host: EMAIL_HOST,
+    port: 465,
+    secure: true,
+    auth: {
+      user: EMAIL_FROM,
+      pass: EMAIL_PASSWORD
+    }
+  },
+  {
+    from: EMAIL_FROM
+  }
+);
+
+transporter
+  .verify()
+  .then(() => success("Email transport works"))
+  .catch(() => error("Unable to verify email transport"));
 
 /**
  * Send a new email
  */
 const sendMail = async (mail: Mail) => {
-  // Send this email;
+  const result = await transporter.sendMail({
+    to: mail.to,
+    cc: mail.cc,
+    bcc: mail.bcc,
+    subject: mail.subject,
+    text: mail.altText,
+    html: mail.message
+  });
+  success("Sent email", mail.to, mail.subject);
+  return result;
 };
 
 /**
  * Send a new email
  */
-export const mail = async (
-  to: number | string,
-  template: string,
-  data: any = {}
-) => {
+export const mail = async (to: string, template: string, data: any = {}) => {
   const altText = render(
     (await readFile(
       join(__dirname, "..", "..", "..", "src", "templates", `${template}.md`)
@@ -42,7 +69,7 @@ export const mail = async (
   const message = marked(altText);
   return await sendMail({
     from: EMAIL_FROM,
-    to: to.toString(),
+    to,
     subject: i18n.en.emails[template] || "",
     message,
     altText
